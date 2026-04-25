ab2e66d5f85c0b0f070f63a84183e170cacc39fe
raffid05 <raffid2005@gmail.com>
2026-04-24 21:47:47 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,6 +1,6 @@
 console.log("IT’S ALIVE!");
 
-function $$(selector, context = document) {
+export function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
